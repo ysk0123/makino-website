e400c25304bec4573d34c5fe5f8edcda3c086e68
Yusuke_sato <bavardage4@gmail.com>
2025-09-02 23:09:53 +0900

    fix: responsive shop section index.page

--- components/shop.js
+++ components/shop.js
@@ -1,15 +1,14 @@
 import styles from "../styles/shop.module.css";
 
 export default function Shop() {
-  return (
-    <div className={styles.shopWrapper}>
+    return (
+      <div className={styles.contentAria}>
         <div className={styles.textContents}>
-          <h2 className={styles.title}>Shop</h2>
-          <a className={styles.link} href="https://makinoart.theshop.jp/">
-            view more
+        <h2 className={styles.title}>Shop</h2>
+          <a  className ={styles.link} href="https://makinoart.theshop.jp/">
+          view more
           </a>
         </div>
-      <img className={styles.shopImg} src="shop.jpg" />
-    </div>
+      </div>
   );
 }
